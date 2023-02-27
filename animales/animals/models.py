f6819d12a541f5cd98dac3b9a3c2301b38b13cf3
Nelsonjs77 <nelsonjuarezsandoval4@gmail.com>
2023-02-27 13:32:42 -0600

    firts commit

---
+++ animales/animals/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settigs
+
+# Create your models here.
+nombreanimal = models.TextField(default=' ', blank=false)
+edadanimal = models.IntegerField(default=' ', blank=false)
+especieanimal = models.TextField(default=' ', blank=false)
+pesoanimal = models.TextField(default=' ', blank=false)
+tamanoanimal = models.TextField(default=' ', blank=false)
+generoanimal = models.TextField(default=' ', blank=false)
+tipoanimal = models.TextField(default=' ', blank=false)
+habitadanimal = models.TextField(default=' ', blank=false)
+alimentacionanimal = models.TextField(default=' ', blank=false)
+longevidadanimal = models.IntegerField(default=' ', blank=false)
